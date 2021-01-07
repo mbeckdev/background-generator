e845756f5aaaf2eed99915607da96825c3251304
mbeckdev <mbeckdev42@gmail.com>
2021-01-06 20:01:45 -0700

    replace vars with lets, make a grid, make  math work for first HEX#

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ function har() {
 }
 har();
 
-var rndColor1;
-var rndColor2;
+// var rndColor1;
+// var rndColor2;
 
 function getRandomNum() {
     let num = 0;
@@ -16,7 +16,36 @@ function getRandomNum() {
     return num;
 }
 
-var places = ["bod", "phone-size"];
+let places = ["bod", "phone-size"];
+
+// VARIABLES TO HELP WITH DECIMAL NUMBERS IN RGB
+let rnd1R, rnd1G, rnd1B;
+let rnd2R, rnd2G, rnd2B;
+
+// VARIABLES TO HELP WITH HEX NUMBERS
+let hex1R, hex1G, hex1B;
+let hex2R, hex2G, hex2B;
+let firstDecDigit, secondDecDigit;
+let firstHexDigit, secondHexDigit;
+
+let deciToHex = {
+    0: '0',
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: 'A',
+    11: 'B',
+    12: 'C',
+    13: 'D',
+    14: 'E',
+    15: 'F',
+}
 
 function colors() {
     rnd1R = getRandomNum();
@@ -38,18 +67,44 @@ function colors() {
             rnd2B + '))';
     }
 
-
     document.getElementById('color1').innerHTML
         = 'rgb( ' +
         rnd1R + ', ' +
         rnd1G + ', ' +
         rnd1B + ' ) ';
 
-
-
     document.getElementById('color2').innerHTML
         = 'rgb( ' +
         rnd2R + ', ' +
         rnd2G + ', ' +
         rnd2B + ' )';
+
+    // TRANSLATE COLORS FROM DECIMAL TO HEX
+    hex1R = translateRGBtoHEX(rnd1R);   //THIS IS A TWO CHAR STRING
+    hex1G = translateRGBtoHEX(rnd1G);
+    hex1B = translateRGBtoHEX(rnd1B);
+    hex2R = translateRGBtoHEX(rnd2R);
+    hex2G = translateRGBtoHEX(rnd2G);
+    hex2B = translateRGBtoHEX(rnd2B);
+
+    //WRITE HEX NUMBERS TO HTML
+    document.getElementById('hex-color1').innerHTML
+        = '#' +
+        hex1R +
+        hex1G +
+        hex1B;
 }
+
+function translateRGBtoHEX(rgbNumber) {
+
+    secondDecDigit = rgbNumber % 16;  //255%16=15 ...
+    firstDecDigit = Math.floor(rgbNumber / 16);  // forget the decimals... how?
+
+    firstHexDigit = deciToHex[firstDecDigit];  //should be a string after this
+    secondHexDigit = deciToHex[secondDecDigit];  //should be a string after this
+
+    // NOW SET THE HEX NUMBERS
+    return firstHexDigit + secondHexDigit;  //THIS IS A STRING
+}
+
+
