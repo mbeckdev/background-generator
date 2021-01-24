0b7c50ffc6dcaa416bdbdb2fde6f3bb36d54ef9e
mbeckdev <mbeckdev42@gmail.com>
2021-01-23 23:31:05 -0700

    Add number associated to slider value

--- app.js
+++ app.js
@@ -224,3 +224,12 @@ function translateRGBtoHSL(rndR, rndG, rndB, letter) {
     }
 
 }
+
+var slider = document.querySelector(".slider");
+var output = document.getElementById("range-num");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
